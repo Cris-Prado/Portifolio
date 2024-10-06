6de3163b23d447b7a5336586fd4c3a490a55bbb9
CRISTIANO PRADO <cristianoprado365@gmail.com>
2024-10-06 19:46:48 -0300

    Version 1.2

--- src/config/js/MenuToogle.js
+++ src/config/js/MenuToogle.js
@@ -1,25 +1,55 @@
-/*
-const hamburger = document.querySelector(".mobileMenu");
-const navMenu = document.querySelector(".list");
-const modeTheme = document.querySelector('.mode')
-const mode = document.querySelector('.mode-night')
-
-
-hamburger.addEventListener("click", () => {
-    hamburger.classList.toggle('active');
-    navMenu.classList.toggle('active');
-})
-
-modeTheme.addEventListener("click", () => {
-    modeTheme.classList.toggle('active');
-    mode.classList.toggle('dark');
-})
-
-*/
 const hamburger = document.querySelector(".mobile");
 const navMenu = document.querySelector(".list");
 
 hamburger.addEventListener("click", () => {
     hamburger.classList.toggle('active');
     navMenu.classList.toggle('active');
-})
+});
+
+// Código para o Search Bar 
+
+const searchInput = document.getElementById("search")
+
+searchInput.addEventListener('input', (event) => {
+    const value = formatString(event.target.value);
+
+    const items = document.querySelectorAll('.item-sear .boxGrup');
+
+    const noResults =document.getElementById('no_results');
+
+    let hasResults = false;
+
+    if(value !== ''){
+        items.forEach(item => {
+            const itemTitle = item.querySelector('.tl1').textContent;
+            const itemData = item.querySelector('.tl2-date').textContent;
+
+            if(formatString(itemTitle).indexOf(value) !== -1 || formatString(itemData).indexOf(value) !== -1){ 
+                item.style.display = 'flex';
+
+                hasResults =  true;
+            } else {
+                item.style.display = 'none';
+            }
+        });
+
+        if (hasResults){
+            noResults.style.display = 'none';
+        } else {
+            noResults.style.display = 'block';
+        }
+    }  else {
+        items.forEach(items => item.style.display = 'flex');
+        
+        noResults.style.display = 'none';
+    }
+       
+});
+
+function formatString(value){
+    return value
+    .toLowerCase()
+    .trim()
+    .normalize('NFD')
+    .replace(/[\u0300-\u036f]/g, '');
+}
